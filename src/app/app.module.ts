60e3a15f172ec43db5511041dce1c294e728ef64
ViktorVerbytskyi <Viktor_Verbytskyi@epam.com>
2023-05-09 10:53:00 +0300

    implemented drug-and-drop for file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { DndDirective } from './directives/dnd.directive';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressComponent,
+    DndDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
